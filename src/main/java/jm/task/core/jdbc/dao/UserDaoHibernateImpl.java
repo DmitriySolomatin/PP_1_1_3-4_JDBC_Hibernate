5b7e38ae11ff85ce79a090bfb3709fbeda7cd2c7
DmitriySolomatin <solomatin@megavega.ru>
2022-09-26 11:14:50 +0300

    rollbacks

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,9 +3,9 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import javax.persistence.criteria.CriteriaQuery;
-import javax.transaction.Transactional;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -14,10 +14,10 @@ public class UserDaoHibernateImpl implements UserDao {
     }
 
     @Override
-    @Transactional
     public void createUsersTable() {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             String sql = "CREATE TABLE IF NOT EXISTS `users` (\n" +
                     "                      `id` INT NOT NULL AUTO_INCREMENT,\n" +
                     "                      `name` VARCHAR(45) NULL,\n" +
@@ -26,63 +26,85 @@ public class UserDaoHibernateImpl implements UserDao {
                     "                      PRIMARY KEY (`id`),\n" +
                     "                      UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);";
             session.createNativeQuery(sql).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
-    @Transactional
     public void dropUsersTable() {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             String sql = "DROP TABLE IF EXISTS users;";
             session.createNativeQuery(sql).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
-    @Transactional
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
         User user = new User(name, lastName, age);
         try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.save(user);
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                e.printStackTrace();
+                transaction.rollback();
+            }
         }
     }
 
     @Override
-    @Transactional
     public void removeUserById(long id) {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.delete(session.load(User.class, id));
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
-    @Transactional
     public List<User> getAllUsers() {
+        List<User> users;
         try (Session session = Util.getSessionFactory().openSession()) {
             CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder().createQuery(User.class);
             criteriaQuery.from(User.class);
-            return session.createQuery(criteriaQuery).getResultList();
+            users = session.createQuery(criteriaQuery).getResultList();
         }
+        return users;
     }
 
     @Override
-    @Transactional
     public void cleanUsersTable() {
         // В задании написано, что методы создания и удаления таблицы должны быть написаны с использованием SQL.
         // Про очистку ничего не сказано. Какой-то встроенный функционал для очистки не нашёл, только SQL.
         // Читать всех пользователей, а потом каждого удалять, как-то не логично.
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             String sql = "TRUNCATE TABLE users;";
             session.createNativeQuery(sql).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 }
